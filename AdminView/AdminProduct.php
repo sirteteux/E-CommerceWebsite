d24c774eed79bb82ae5f3c45a66de1b4429febf1
sirteteux <60591186+sirteteux@users.noreply.github.com>
2020-11-19 17:52:56 +0800

    Update AdminProduct.php

--- AdminView/AdminProduct.php
+++ AdminView/AdminProduct.php
@@ -30,6 +30,10 @@
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 	
@@ -203,7 +207,7 @@
 		</div>
 	</header>
 
-		<div class="container">   
+ <div class="container">   
    <div class="panel panel-default">
     <div class="panel-heading">
      <div class="row">
